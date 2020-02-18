13a1a26bffff7b484ec66c48b200f6f8f40ab2a2
Łukasz Zabuski <lukasz@biuro.bcweb.pl>
2020-02-18 22:19:39 +0100

    v1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Person = ({ img, name, job, children }) => {
+  return (
+    <article className="person">
+      <img
+        src={`https://randomuser.me/api/portraits/thumb/men/${img}.jpg`}
+        alt="person"
+      />
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+      {children}
+    </article>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const PersonList = () => {
+  return (
+    <section className="person-list">
+      <Person img="34" name="john" job="developer"></Person>
+      <Person img="28" name="bob" job="designer">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores,
+          possimus.
+        </p>
+      </Person>
+      <Person img="15" name="nick" job="bot"></Person>
+    </section>
+  );
+};
+
+ReactDOM.render(<PersonList></PersonList>, document.getElementById("root"));
